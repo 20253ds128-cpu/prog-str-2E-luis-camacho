b38548d463713cfb369485389746c5bd62bd843c
Luis Fernando Camacho <20253ds128@utez.edu.mx>
2026-02-26 20:57:21 -0600

    Modificación de App

---
+++ PracticaCRUDLuisCamacho/src/Aplication.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Aplication {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        int opcion = 0;
+
+
+        do {
+
+            System.out.println("-----MENÚ-----");
+            System.out.println("1) Registrar curso");
+            System.out.println("2) Mostrar todos los cursos");
+            System.out.println("3) Buscar curso por ID");
+            System.out.println("4) Actualizar curso");
+            System.out.println("5) Eliminar curso");
+            System.out.println("6) Salir");
+            System.out.println("Opción: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+
+                
+
+
+
+            }
+
+        } while (opcion != 6);
+
+
+    }
+}
