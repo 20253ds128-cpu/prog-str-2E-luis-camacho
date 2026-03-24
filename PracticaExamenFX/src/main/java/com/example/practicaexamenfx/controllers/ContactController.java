87d2abe3dd2bdea23d159c7f1b860f2a0fb71116
Luis Camacho <20253ds128@utez.edu.mx>
2026-03-24 09:31:56 -0600

    PracticaContactosFX

--- PracticaExamenFX/src/main/java/com/example/practicaexamenfx/controllers/ContactController.java
+++ PracticaExamenFX/src/main/java/com/example/practicaexamenfx/controllers/ContactController.java
@@ -1,34 +1,73 @@
 package com.example.practicaexamenfx.controllers;
 
+import com.example.practicaexamenfx.model.Contacto;
+import com.example.practicaexamenfx.services.ContactService;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
+import java.util.List;
+
+private ContactService service = new ContactService();
 
 public class ContactController {
 
-    private String[] arrParentescos = {   "Padre"+
-            "Madre"+
-            "Hermano"+
-            "Hermana"+
-            "Abuelo"+
-            "Abuela"+
-            "Tio"+
-            "Tia"};
+    private String[] arrParentescos = {
+            "Padre",
+            "Madre",
+            "Hermano",
+            "Hermana",
+            "Abuelo",
+            "Abuela",
+            "Tio",
+            "Tia"
+    };
+
 
     @FXML
-    private ComboBox<String> arrParentesco;
-    private ObservableList<String> listObservableParent = FXCollections.observableArrayList();
+    private TextField txtName;
 
+    @FXML
+    private TextField txtTel;
+
+    @FXML
+    private ListView<Contacto> listObservableContacts = FXCollections.observableArrayList();
+    private List<Contacto> listContacts = new ArrayList<>();
+
+    @FXML
+    private ComboBox<String> cbParentesco;
+    private ObservableList<String> listObservableParent = FXCollections.observableArrayList();
+    private ObservableList<String> ObservableParent;
+
+    private ContactService service = new ContactService();
 
     @FXML
     public void initialize () {
         listObservableParent.setAll(arrParentescos);
-
+        cbParentesco.setItems(ObservableParent);
+        
     }
 
+    @FXML
+    public void onAddContact() {
+        String name = txtName.getText().trim();
+        String tel = txtTel.getText().trim();
+
+        String parent = cbParentesco.getValue();
+
+        Contacto contact = new Contacto(name,tel,parent);
+        service.addContacts(contact);
+
+        listObservableContacts.setAll(service.getAllContacts());
 
 
+        System.out.println(service.getAllContacts());
 
+
+    }
 
 }
--- PracticaExamenFX/src/main/java/module-info.java
+++ PracticaExamenFX/src/main/java/module-info.java
@@ -4,5 +4,13 @@ module com.example.practicaexamenfx {
 
 
     opens com.example.practicaexamenfx to javafx.fxml;
+    opens com.example.practicaexamenfx.controllers to javafx.fxml;
+    opens com.example.practicaexamenfx.model to javafx.fxml;
+    opens com.example.practicaexamenfx.services to javafx.fxml;
+
     exports com.example.practicaexamenfx;
+    exports com.example.practicaexamenfx.controllers;
+    exports com.example.practicaexamenfx.model;
+    exports com.example.practicaexamenfx.services;
+
 }
